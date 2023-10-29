ea9cc5eae6dcc5001a52c18c1d48217c78c2cd7d
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-10-29 18:24:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removecolor () {
+	var selectElement = document.getElementById('colorSelect');
+	var selectedValue = selectElement.value;
+	for(var i=0; i<selectElement.length; i++);{
+        if (selectElement.options[i].value === selectedValue) {
+          selectElement.remove(i);
+          break;
+        }
+      }
+}
