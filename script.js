1df4f47d53907ca31af22e207f4c892e4e9ba24f
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-10-29 18:31:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 //your JS code here. If required.
-function removecolor () {
-	var selectElement = document.getElementById('colorSelect');
-	var selectedValue = selectElement.value;
-	for(var i=0; i<selectElement.length; i++);{
-        if (selectElement.options[i].value === selectedValue) {
-          selectElement.remove(i);
-          break;
-        }
-      }
-}
+const removeColor = () => {
+  const select = document.getElementById('colorSelect');
+  select.remove(select.selectedIndex);
+};
+
+document.querySelector('input[type="button"]').addEventListener('click', removeColor);
